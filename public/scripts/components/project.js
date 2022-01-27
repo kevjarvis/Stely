9c9a957a269ef65b27013122d9fbe0a497013f0a
Kevin Castillo <kcastilloc1@uft.edu>
2022-01-27 00:57:08 -0300

    Se optimizaron todas las imagenes del proyecto closes #16

--- public/scripts/components/project.js
+++ public/scripts/components/project.js
@@ -1,6 +1,9 @@
 export const Project  = (img, projectName, anualReturns, investors) => {
   return `<article class="project">
-            <img class="project-img" src=${img} alt="">
+            <picture class="project-img">
+              <source media="(min-width: 768px)" srcset="${img}@desktop.jpg">
+              <img class="project-img" src="${img}@mobile.jpg" alt="">
+            </picture>
             <div class="project-description">
               <span class="project-name">${projectName}</span>
               <div class="project-info">
